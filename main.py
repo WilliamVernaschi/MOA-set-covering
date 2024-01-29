f0b92493e7c06964becf5a090879a5f5a6405304
William Vernaschi <williamdavi13@gmail.com>
2024-01-29 10:32:41 -0300

    coisas

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import random
 from dataclasses import dataclass
 from functools import cmp_to_key
-import random
 from math import ceil, log2, log as ln, inf
 from more_itertools import ilen as count
 from copy import deepcopy
+from typing import List, TypeVar
+
+# Define a generic type variable
+T = TypeVar('T')
 
 WEIGHT_SCALING_FACTOR = 100
 
@@ -19,6 +23,12 @@ class Matrix:
     num_columns : int
     num_rows : int
 
+def flatten(xss : list[list[T]]) -> list[T]:
+    return [
+        x
+        for xs in xss
+        for x in xs
+    ]
 
 # returns a list of a list of strings
 def read_input(file_name : str) -> list[list[str]]:
@@ -113,6 +123,7 @@ def min_set_cover(matrix : Matrix) -> tuple[float, list[int]]:
                 continue
             best_pair = min(best_pair, (random_greedy_cost(column.cost, num_rows_covered_by_column), column.idx))
 
+
         chosenColumn = best_pair[1]
 
         assert chosenColumn != -1 # If this assertion fails, there is no viable solution.
@@ -130,21 +141,25 @@ def min_set_cover(matrix : Matrix) -> tuple[float, list[int]]:
     return (get_total_weight(S, matrix), sorted(S))
 
 def min_set_cover_search(matrix : Matrix, solution : list[int]) -> tuple[float, list[int]]:
-    # Set d = 0,
-    rho1 = random.uniform(0.05, 0.7)
-    rho2 = random.uniform(1.1, 2)
-    # D = rho1*N(S), 
-    D = ceil(rho1 * len(solution))
-    # E = rho2*Q(s)
-    E = ceil(rho2 * max(matrix.data[column_idx].cost for column_idx in solution))
+    ρ1 = random.uniform(0.1, 0.4)
+    ρ2 = random.uniform(1.1, 2)
 
-    random.shuffle(solution)
+    D = ceil(ρ1 * len(solution))
+    # D := number of columns to be removed
+    E = ceil(ρ2 * max(matrix.data[column_idx].cost for column_idx in solution))
+    # E := weight limit on the columns that will be added
+
+
     total_coverings = get_total_coverings(solution, matrix)
 
-    R = {c for c in range(matrix.num_columns)}
+    # R := set of all columns that are not in S
+    R = {c for c in range(matrix.num_columns)} 
     for c in solution:
         R.remove(c)
 
+    # instead of removing D random columns, just shuffle the solution list
+    # and get the last D.
+    random.shuffle(solution)
     for _ in range(D):
 
         random_column = solution[-1]
@@ -156,24 +171,21 @@ def min_set_cover_search(matrix : Matrix, solution : list[int]) -> tuple[float, 
         solution.pop()
 
     uncovered_rows = {row_idx for row_idx in range(matrix.num_rows) if total_coverings[row_idx] == 0}
-
+    # uncovered_rows := set of all rows that aren't covered by any column
 
     while len(uncovered_rows) > 0:
-        substitute_column_candidates = [column_idx for column_idx in R if matrix.data[column_idx].cost <= E]
 
+        substitute_column_candidates = [column_idx for column_idx in R if matrix.data[column_idx].cost <= E]
 
         α = [sum(1 for i in matrix.data[column_idx].rows if i in uncovered_rows) \
                 for column_idx in substitute_column_candidates]
-
         β = [matrix.data[col_idx].cost/αj if αj > 0 else inf for col_idx,αj in enumerate(α)]
 
-        bmin = min(βj for βj in β)
+        βmin = min(βj for βj in β)
 
-        assert bmin < inf
+        assert βmin < inf # if this assertion fails, there was a bad choice of ρ2
 
-
-        K = [c for idx,c in enumerate(substitute_column_candidates) if β[idx] == bmin]
-
+        K = [c for idx,c in enumerate(substitute_column_candidates) if β[idx] == βmin]
         new_chosen_column = random.choice(K)
 
         for row in matrix.data[new_chosen_column].rows:
@@ -184,73 +196,40 @@ def min_set_cover_search(matrix : Matrix, solution : list[int]) -> tuple[float, 
         solution.append(new_chosen_column)
         R.remove(new_chosen_column)
 
-
     remove_useless_columns(solution, matrix)
 
     return (get_total_weight(solution, matrix), solution)
 
 
-# this is really bad, will remove it
 def min_set_cover_constructive2(matrix : Matrix):
-    num_groups = random.randint(15,25)
+    row_occurence = [[] for row in range(matrix.num_rows)]
+    covered = [False for row in range(matrix.num_rows)]
 
-    group_size = ceil(matrix.num_columns / num_groups)
+    for c in matrix.data:
+        for r in c.rows:
+            row_occurence[r].append(c)
 
-    matrix.data.sort(key=cmp_to_key(lambda ra, rb : sum(r for r in ra.rows) - sum(r for r in rb.rows)))
+    rows_covered = 0
+    S = []
+    while rows_covered < matrix.num_rows:
+        minC = min(len(pos) for idx,pos in enumerate(row_occurence) if not covered[idx])
+        candidates = flatten([pos for idx,pos in enumerate(row_occurence) if not covered[idx] and len(pos) == minC])
+        candidates.sort(key=cmp_to_key(lambda a,b : int(a.cost - b.cost)))
+        random.shuffle(candidates)
+        chosen = candidates[0]
 
-    groups = [list() for _ in range(num_groups)]
+        S.append(chosen.idx)
+        for r in chosen.rows:
+            if not covered[r]:
+                covered[r] = True
+                rows_covered += 1
 
-    for idx, column in enumerate(matrix.data):
-        groups[idx%num_groups].append(column)
+        for idx,rc in enumerate(row_occurence):
+            row_occurence[idx] = [c for c in row_occurence[idx] if c.idx != chosen.idx]
 
+    remove_useless_columns(S, matrix)
 
-    groups_selected = [False for _ in range(num_groups)]
-
-    def get_solution(groups_selected : list[bool]) -> tuple[float, list[int]]:
-        columns = [column.idx
-                   for column_group in (groups[i] for i in range(num_groups) if groups_selected[i])
-                   for column in column_group]
-
-        if not valid_solution(columns, matrix):
-            return (inf, [])
-
-
-        return (sum(matrix.data[column_idx].cost for column_idx in columns), columns)
-
-
-    ans = inf
-
-    print(num_groups)
-    def try_all(group_idx : int):
-        if group_idx == num_groups:
-            return get_solution(groups_selected)[0]
-        else:
-            groups_selected[group_idx] = True
-            a1 = try_all(group_idx+1)
-            groups_selected[group_idx] = False
-            a2 = try_all(group_idx+1)
-            return min(a1, a2)
-
-    return try_all(0)/100
-
-
-
-
-
-
-
-
-
-
-def valid_solution(solution : list[int], matrix : Matrix):
-    rows = [False for _ in range(matrix.num_rows)]
-
-    for c in solution:
-        for r in matrix.data[c].rows:
-            rows[r] = True
-
-
-    return all(f for f in rows)
+    return (get_total_weight(S, matrix), S)
 
 
 
@@ -267,19 +246,20 @@ if __name__ == "__main__":
     matrix = parseInput(inp)
 
     best_solution : tuple[float, list[int]] = (inf, [])
-    total_cost, chosen_columns = min_set_cover(matrix)
 
-    for _ in range(20000):
+    for _ in range(5):
+        total_cost, chosen_columns = min_set_cover_constructive2(matrix)
+        print(_)
+        for __ in range(5000):
 
-        current_solution = min_set_cover_search(matrix, chosen_columns)
+            current_solution = min_set_cover_search(matrix, chosen_columns)
 
-        if current_solution[0] < best_solution[0]:
-            best_solution = deepcopy(current_solution)
-            print(best_solution)
+            if current_solution[0] < best_solution[0]:
+                best_solution = deepcopy(current_solution)
+                print(best_solution)
 
-        chosen_columns = deepcopy(best_solution[1])
+            chosen_columns = deepcopy(best_solution[1])
 
-    assert valid_solution(best_solution[1], matrix)
     print(f"Custo: {best_solution[0]}")
     print(f"Colunas: {sorted([c+1 for c in best_solution[1]])}")
 
