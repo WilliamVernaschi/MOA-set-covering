2cd73c5d3cf3a71f935d6c62f308d184a4b2436f
William Vernaschi <williamvernaschi@gmail.com>
2024-02-14 15:02:33 -0300

    entrega final

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import random
 from dataclasses import dataclass
 from functools import cmp_to_key
 from math import ceil, log2, log as ln, inf
-from more_itertools import ilen as count
 from copy import deepcopy
-from typing import List, TypeVar
+from typing import TypeVar
 
-# Define a generic type variable
 T = TypeVar('T')
 
+# Since all costs have two decimal places, we multiply them by 100 to avoid floating point errors
 WEIGHT_SCALING_FACTOR = 100
 
 @dataclass
@@ -23,20 +22,55 @@ class Matrix:
     num_columns : int
     num_rows : int
 
-def flatten(xss : list[list[T]]) -> list[T]:
+@dataclass
+class Solution:
+    cost : float
+    columns : list[int]
+
+def flatten(xss: list[list[T]]) -> list[T]:
+    """
+    Flattens a list of lists into a single list.
+
+    Parameters:
+        xss (list[list[T]]): The list of lists to be flattened.
+
+    Returns:
+        list[T]: The flattened list.
+    """
     return [
         x
         for xs in xss
         for x in xs
     ]
 
-# returns a list of a list of strings
-def read_input(file_name : str) -> list[list[str]]:
+
+def read_input(file_name: str) -> list[list[str]]:
+    """
+    Read the input file and return a list of lists containing the data.
+
+    Parameters:
+        file_name (str): The name of the input file.
+
+    Returns:
+        list[list[str]]: A list of lists containing the data from the input file.
+    """
     with open(file_name, "r") as f:
         return [line.split() for line in f]
 
-# given the input, returns the Matrix
-def parseInput(inp : list[list[str]]) -> Matrix:
+
+def parse_input(inp : list[list[str]]) -> Matrix:
+    """
+    Parses the input and returns a Matrix object.
+
+    Parameters:
+        inp (list[list[str]]): The input data.
+
+    Returns:
+        Matrix: The parsed matrix object.
+
+    Raises:
+        ValueError: If the input format is invalid.
+    """
     num_rows = int(inp[0][1])
     num_columns = int(inp[1][1])
 
@@ -51,11 +85,19 @@ def parseInput(inp : list[list[str]]) -> Matrix:
 
     return Matrix(data=columns, num_columns=num_columns, num_rows=num_rows)
 
-# returns the list total_coverings
-# total_coverings[i] := how many columns in `solution` cover row `i`
 def get_total_coverings(solution : list[int], matrix : Matrix) -> list[int]:
+    """
+    Returns total_coverings, where, total_coverings[i] := how many columns in `solution` cover row `i`
 
-    total_coverings = [0 for i in range(matrix.num_rows)]
+    Parameters:
+        solution (list[int]): The selected columns in the solution.
+        matrix (Matrix): The matrix containing the data.
+
+    Returns:
+        list[int]: A list containing the total number of coverings for each row.
+    """
+
+    total_coverings = [0 for _ in range(matrix.num_rows)]
 
     for column in solution:
         for row in matrix.data[column].rows:
@@ -64,11 +106,20 @@ def get_total_coverings(solution : list[int], matrix : Matrix) -> list[int]:
     return total_coverings
 
 
-# given a solution, consider the columns in decreasing 
-# order of cost, if a column, when removed, still results
-# in a valid solution, remove it. Modifies the solution list in-place
-def remove_useless_columns(solution : list[int], matrix : Matrix) -> None:
 
+def remove_useless_columns(solution : list[int], matrix : Matrix) -> None:
+    """
+    Removes useless columns from the solution based on the given matrix, this is done
+    by considering the columns in decreasing order of cost, if a column, when removed, still results
+    in a valid solution, remove it.
+
+    Parameters:
+        solution (list[int]): The list of column indices representing the solution.
+        matrix (Matrix): The matrix containing the data.
+
+    Returns:
+        None
+    """
     total_coverings = get_total_coverings(solution, matrix)
 
     # sort solution by cost
@@ -85,8 +136,17 @@ def remove_useless_columns(solution : list[int], matrix : Matrix) -> None:
     solution[:] = [c for c in solution if c not in useless]
 
 
-# given a list of chosen columns, returns the total weight of the solution
 def get_total_weight(solution : list[int], matrix : Matrix) -> float:
+    """
+    Calculates the total weight of a given solution based on the cost values in the matrix.
+
+    Parameters:
+        solution (list[int]): The indices of the selected columns in the solution.
+        matrix (Matrix): The matrix containing the cost values.
+
+    Returns:
+        float: The total weight of the solution.
+    """
     return sum(matrix.data[column_idx].cost for column_idx in solution)/WEIGHT_SCALING_FACTOR
 
 greedy_options = [
@@ -98,22 +158,26 @@ greedy_options = [
         lambda cj, kj : cj/(kj*kj),
         lambda cj, kj : cj**(1/2)/(kj*kj)]
 
-# sorts one of the greedy functions and uses it
-def random_greedy_cost(weight : float, num_rows_covered_by_column : int) -> float:
-    return random.choice(greedy_options)(weight, num_rows_covered_by_column)
-
-def greedy_cost(weight : float, num_rows_covered_by_column : int) -> float:
-    return weight/num_rows_covered_by_column
 
 
-# returns a pair of the total cost and the chosen columns
-def min_set_cover(matrix : Matrix) -> tuple[float, list[int]]:
+def min_set_cover_constructive(matrix : Matrix) -> Solution:
+    """
+    Construct a minimum set cover solution using a constructive greedy algorithm.
+
+    Parameters:
+        matrix (Matrix): The input matrix representing the set cover problem.
+
+    Returns:
+        Solution: The minimum set cover solution, containing the total weight and the sorted set of chosen columns.
+    """
     S = [] # solution set
     num_covered = 0 # number of rows already covered
     row_covered = [False for i in range(matrix.num_rows)]
     # row_covered[i] := has row `i` been covered by some column?
 
     while num_covered < matrix.num_rows:
+        greedy = random.choice(greedy_options)
+
         best_pair = (inf, -1) # pair of cost of choosing a column and its index
                               # initially, no column is chosen.
 
@@ -121,7 +185,7 @@ def min_set_cover(matrix : Matrix) -> tuple[float, list[int]]:
             num_rows_covered_by_column = sum(1 for row in column.rows if not row_covered[row])
             if num_rows_covered_by_column == 0:
                 continue
-            best_pair = min(best_pair, (random_greedy_cost(column.cost, num_rows_covered_by_column), column.idx))
+            best_pair = min(best_pair, (greedy(column.cost, num_rows_covered_by_column), column.idx))
 
 
         chosenColumn = best_pair[1]
@@ -138,11 +202,25 @@ def min_set_cover(matrix : Matrix) -> tuple[float, list[int]]:
 
     remove_useless_columns(S, matrix)
 
-    return (get_total_weight(S, matrix), sorted(S))
+    return Solution(get_total_weight(S, matrix), sorted(S))
 
-def min_set_cover_search(matrix : Matrix, solution : list[int]) -> tuple[float, list[int]]:
-    ρ1 = random.uniform(0.1, 0.8)
-    ρ2 = random.uniform(1.1, 2)
+def min_set_cover_search(matrix : Matrix, solution : list[int], ρ1 : float, ρ2 : float) -> Solution:
+    """
+    Search for a minimum set cover solution using a local search algorithm, as described by 
+    Jacobs, L. W. and Brusco.
+
+    Parameters:
+        matrix (Matrix): The matrix representing the set cover problem.
+        solution (list[int]): The initial solution.
+        ρ1 (float): The parameter controlling the number of columns to be removed.
+        ρ2 (float): The parameter controlling the weight limit on the columns that will be added.
+
+    Returns:
+        Solution: The minimum set cover solution.
+
+    Raises:
+        None.
+    """
 
     D = ceil(ρ1 * len(solution))
     # D := number of columns to be removed
@@ -180,10 +258,11 @@ def min_set_cover_search(matrix : Matrix, solution : list[int]) -> tuple[float, 
         α = [sum(1 for i in matrix.data[column_idx].rows if i in uncovered_rows) \
                 for column_idx in substitute_column_candidates]
         β = [matrix.data[col_idx].cost/αj if αj > 0 else inf for col_idx,αj in enumerate(α)]
-
+        
+        if len(β) == 0 or min(βj for βj in β) == inf:
+            return Solution(inf, []) # There was a bad choice of ρ2
+        
         βmin = min(βj for βj in β)
-
-        assert βmin < inf # if this assertion fails, there was a bad choice of ρ2
 
         K = [c for idx,c in enumerate(substitute_column_candidates) if β[idx] == βmin]
         new_chosen_column = random.choice(K)
@@ -198,38 +277,102 @@ def min_set_cover_search(matrix : Matrix, solution : list[int]) -> tuple[float, 
 
     remove_useless_columns(solution, matrix)
 
-    return (get_total_weight(solution, matrix), solution)
+    return Solution(get_total_weight(solution, matrix), solution)
 
 
-def min_set_cover_constructive2(matrix : Matrix):
-    row_occurence = [[] for row in range(matrix.num_rows)]
-    covered = [False for row in range(matrix.num_rows)]
+def min_set_cover_constructive2(matrix : Matrix) -> Solution:
+    """
+    Construct a minimum set cover solution by cosidering the columns in increasing weight.
 
-    for c in matrix.data:
-        for r in c.rows:
-            row_occurence[r].append(c)
+    Parameters:
+        matrix (Matrix): The input matrix representing the set cover problem.
 
-    rows_covered = 0
-    S = []
-    while rows_covered < matrix.num_rows:
-        minC = min(len(pos) for idx,pos in enumerate(row_occurence) if not covered[idx])
-        candidates = flatten([pos for idx,pos in enumerate(row_occurence) if not covered[idx] and len(pos) == minC])
-        candidates.sort(key=cmp_to_key(lambda a,b : int(a.cost - b.cost)))
-        random.shuffle(candidates)
-        chosen = candidates[0]
+    Returns:
+        Solution: The minimum set cover solution.
 
-        S.append(chosen.idx)
-        for r in chosen.rows:
-            if not covered[r]:
-                covered[r] = True
-                rows_covered += 1
+    """
+    S = []  # solution set
+    num_covered = 0  # number of rows already covered
+    row_covered = [False for i in range(matrix.num_rows)]
+    # row_covered[i] := has row `i` been covered by some column?
 
-        for idx,rc in enumerate(row_occurence):
-            row_occurence[idx] = [c for c in row_occurence[idx] if c.idx != chosen.idx]
+    # sort columns by weight in increasing order
+    sorted_columns = sorted(matrix.data, key=lambda column: column.cost)
 
-    remove_useless_columns(S, matrix)
+    for column in sorted_columns:
+        for row in column.rows:
+            if not row_covered[row]:
+                row_covered[row] = True
+                num_covered += 1
 
-    return (get_total_weight(S, matrix), S)
+        S.append(column.index)
+
+        if num_covered == matrix.num_rows:
+            break
+
+    return Solution(get_total_weight(S, matrix), S)
+
+def best_improvement(solution : Solution, n : int) -> Solution:
+    """
+    Improves the given solution by applying a local search algorithm n times and
+    finding the best improvement out of every n/20 searches.
+
+    Parameters:
+        solution (Solution): The initial solution to be improved.
+        n (int): The number of iterations for the local search algorithm.
+
+    Returns:
+    Solution: The improved solution.
+    """
+    
+    improvement = deepcopy(solution)
+    for i in range(n):
+        ρ1 = random.uniform(0.4, 0.9)
+        ρ2 = random.uniform(0.9, 1.5)
+        current_search = min_set_cover_search(matrix, deepcopy(solution.columns), ρ1, ρ2)
+        if current_search.cost < improvement.cost:
+            improvement = deepcopy(current_search)
+        if i % (n//20) == 0 and improvement.cost < solution.cost:
+            solution = deepcopy(improvement)
+
+    return solution
+
+
+def next_improvement(solution : Solution, n : int) -> Solution:
+    """
+    Improves the given solution by applying a local search algorithm n times and
+    finding the next improvement.
+
+    Parameters:
+        solution (Solution): The initial solution to be improved.
+        n (int): The number of iterations for the local search algorithm.
+
+    Returns:
+    Solution: The improved solution.
+    """
+
+    for _ in range(n):
+        ρ1 = random.uniform(0.4, 0.9)
+        ρ2 = random.uniform(0.9, 1.5)
+        current_search = min_set_cover_search(matrix, deepcopy(solution.columns), ρ1, ρ2)
+        if current_search.cost < solution.cost:
+            solution = deepcopy(current_search)
+
+    return solution
+
+def solve(matrix : Matrix) -> Solution:
+    """
+    Solves the set covering problem using a constructive heuristic and local search.
+
+    Parameters:
+        matrix (Matrix): The input matrix representing the set covering problem.
+
+    Returns:
+        Solution: The best solution found by the algorithm.
+    """
+    initial_solution = min_set_cover_constructive(matrix)
+    
+    return next_improvement(initial_solution, 1000)
 
 
 
@@ -243,25 +386,12 @@ if __name__ == "__main__":
     
     input_file = sys.argv[1]
     inp = read_input(input_file)
-    matrix = parseInput(inp)
+    matrix = parse_input(inp)
 
-    best_solution : tuple[float, list[int]] = (inf, [])
+    solution = solve(matrix)
 
-    for _ in range(10):
-        total_cost, chosen_columns = min_set_cover(matrix)
-        print(_)
-        for __ in range(10000):
-
-            current_solution = min_set_cover_search(matrix, chosen_columns)
-
-            if current_solution[0] < best_solution[0]:
-                best_solution = deepcopy(current_solution)
-                print(best_solution)
-
-            chosen_columns = deepcopy(current_solution[1])
-
-    print(f"Custo: {best_solution[0]}")
-    print(f"Colunas: {sorted([c+1 for c in best_solution[1]])}")
+    print(f"Custo: {solution.cost}")
+    print(f"Colunas: {sorted([c+1 for c in solution.columns])}")
 
 
     
